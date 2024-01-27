d15e2c20782f97f69965376418e53d1b443d08f6
Artem <Artem.kha4atrian@gmail.com>
2024-01-27 19:18:34 +0200

    Added methods into class Battle.Need to tests.

--- src/main/java/org/painandsuffer/battle/AdventurerFactory.java
+++ src/main/java/org/painandsuffer/battle/AdventurerFactory.java
@@ -2,18 +2,20 @@ package org.painandsuffer.battle;
 
 import org.painandsuffer.characters.Adventurer;
 
-import java.lang.reflect.InvocationTargetException;
 
 public class AdventurerFactory {
-    public static Adventurer createAdventurer(String name, AdventurerClass adventurerClass)
-            throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    public static Adventurer createAdventurer(String name, AdventurerClass adventurerClass) {
         Object createdAdventurer = createAdventurerByClass(name, adventurerClass);
         return createdAdventurer instanceof Adventurer ? (Adventurer) createdAdventurer : null;
     }
 
-    private static Object createAdventurerByClass(String name, AdventurerClass adventurerClass)
-            throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        return Class.forName("org.painandsuffer.characters.".concat(adventurerClass.toString())).getDeclaredConstructor(String.class).newInstance(name);
+    private static Object createAdventurerByClass(String name, AdventurerClass adventurerClass) {
+        try {
+            return Class.forName("org.painandsuffer.characters.".concat(adventurerClass.toString())).getDeclaredConstructor(String.class).newInstance(name);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
--- src/main/java/org/painandsuffer/battle/Battle.java
+++ src/main/java/org/painandsuffer/battle/Battle.java
@@ -1,6 +1,50 @@
 package org.painandsuffer.battle;
 
+import org.painandsuffer.characters.Adventurer;
+
+import java.util.Scanner;
+
 public class Battle {
+    Scanner scanner = new Scanner(System.in);
+    public void provideTwoPlayersPvP(){
+        greetings();
+        providePvP(getAdventurer(),getAdventurer());
+    }
+
+    private void greetings() {
+    }
+
+    private void providePvP(Adventurer playerOne, Adventurer playerTwo){
+        System.out.println("");
+        while (playerOne.getHealth() !=0 || playerTwo.getHealth() !=0) {
+            playerOne.attack(playerTwo);
+            playerTwo.attack(playerOne);
+        }
+        if (playerOne.getHealth() > 0){
+            System.out.println("");
+        }
+        else System.out.println("");
+    }
+
+    private Adventurer getAdventurer(){
+        return AdventurerFactory.createAdventurer(getAdventurerName(),getAdventurerClass());
+    }
+
+    private AdventurerClass getAdventurerClass(){
+        System.out.println("");
+        try {
+            return AdventurerClass.valueOf(scanner.next());
+        } catch (IllegalArgumentException argumentException){
+            System.out.println("");
+            return getAdventurerClass();
+        }
+    }
+
+    private String getAdventurerName(){
+        System.out.println();
+        return scanner.nextLine();
+    }
+
 
 
 
